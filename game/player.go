345b44b24ffc61daff93d3e48c0ef8cd7034ca25
Andrew Cranston <andrew.cranston@digio.com.au>
2023-01-09 18:47:18 +1100

    Fix accel calculations

--- game/player.go
+++ game/player.go
@@ -1,154 +1,301 @@
 package game
 
 import (
-    "github.com/faiface/pixel/pixelgl"
-    "math"
+	"github.com/faiface/pixel/pixelgl"
+	"math"
 )
 
 type Player struct {
-    view       *RenderView
-    game       *GameInstance
-    controller *PlayerController
+	view       *RenderView
+	game       *GameInstance
+	controller *PlayerController
 }
 
 func (p *Player) update(timeDelta float64) {
-    p.view.render()
+	p.view.render()
 }
 
 func (p *Player) render(timeDelta float64) {
-    p.view.render()
+	p.view.render()
 }
 
 func (p *Player) processInput(win *pixelgl.Window, timeDelta float64) {
-    if win.Pressed(pixelgl.KeyUp) || win.Pressed(pixelgl.KeyW) {
-        p.moveForward(3.5 * timeDelta)
-    }
+	p.processForwardBackAcceleration(win)
 
-    if win.Pressed(pixelgl.KeyA) {
-        p.moveLeft(3.5 * timeDelta)
-    }
+	p.processLeftRightAcceleration(win)
 
-    if win.Pressed(pixelgl.KeyDown) || win.Pressed(pixelgl.KeyS) {
-        p.moveBackwards(3.5 * timeDelta)
-    }
+	if win.Pressed(pixelgl.KeyRight) {
+		p.turnRight(1.2 * timeDelta)
+	}
 
-    if win.Pressed(pixelgl.KeyD) {
-        p.moveRight(3.5 * timeDelta)
-    }
+	if win.Pressed(pixelgl.KeyLeft) {
+		p.turnLeft(1.2 * timeDelta)
+	}
 
-    if win.Pressed(pixelgl.KeyRight) {
-        p.turnRight(1.2 * timeDelta)
-    }
+	mouseVector := win.MousePosition().Sub(win.MousePreviousPosition())
+	if mouseVector.X > 0 {
+		p.turnRight(mouseVector.X * 0.01)
+	} else {
+		p.turnLeft(mouseVector.X * -0.01)
+	}
+}
 
-    if win.Pressed(pixelgl.KeyLeft) {
-        p.turnLeft(1.2 * timeDelta)
-    }
+func (p *Player) processForwardBackAcceleration(win *pixelgl.Window) {
+	accelTriggered := false
+	if win.Pressed(pixelgl.KeyUp) || win.Pressed(pixelgl.KeyW) {
+		p.accelerateForward()
+		accelTriggered = true
+	}
 
-    mouseVector := win.MousePosition().Sub(win.MousePreviousPosition())
-    if mouseVector.X > 0 {
-        p.turnRight(mouseVector.X * 0.01)
-    } else {
-        p.turnLeft(mouseVector.X * -0.01)
-    }
+	if win.Pressed(pixelgl.KeyDown) || win.Pressed(pixelgl.KeyS) {
+		p.accelerateBackward()
+		accelTriggered = true
+	}
+
+	if !accelTriggered {
+		if p.view.velocity > 0 {
+			p.view.velocity += backward_acceleration
+			if p.view.velocity < 0 {
+				p.view.velocity = 0
+			}
+		} else if p.view.velocity < 0 {
+			p.view.velocity -= backward_acceleration
+			if p.view.velocity > 0 {
+				p.view.velocity = 0
+			}
+		}
+	}
+}
+
+func (p *Player) processLeftRightAcceleration(win *pixelgl.Window) {
+	accelTriggered := false
+	if win.Pressed(pixelgl.KeyA) {
+		p.accelerateLeft()
+		accelTriggered = true
+	}
+
+	if win.Pressed(pixelgl.KeyD) {
+		p.accelerateRight()
+		accelTriggered = true
+	}
+
+	if !accelTriggered {
+		if p.view.horizontalVelocity > 0 {
+			p.view.horizontalVelocity += backward_acceleration
+			if p.view.horizontalVelocity < 0 {
+				p.view.horizontalVelocity = 0
+			}
+		} else if p.view.horizontalVelocity < 0 {
+			p.view.horizontalVelocity -= backward_acceleration
+			if p.view.horizontalVelocity > 0 {
+				p.view.horizontalVelocity = 0
+			}
+		}
+	}
 }
 
 func (p *Player) moveForward(s float64) {
-    mapData := p.game.mapData
-    _, ok := interface{}(mapData).([][]int)
+	mapData := p.game.mapData
+	_, ok := interface{}(mapData).([][]int)
 
-    if ok {
+	if ok {
 
-        if p.view.distanceToWall > 0.3 {
-            if mapData[int(p.view.position.X+p.view.direction.X*s)][int(p.view.position.Y)] == 0 {
-                p.view.position.X += p.view.direction.X * s
-            }
+		if p.view.distanceToWall > 0.3 {
+			if mapData[int(p.view.position.X+p.view.direction.X*s)][int(p.view.position.Y)] == 0 {
+				p.view.position.X += p.view.direction.X * s
+			}
 
-            if mapData[int(p.view.position.X)][int(p.view.position.Y+p.view.direction.Y*s)] == 0 {
-                p.view.position.Y += p.view.direction.Y * s
-            }
-        }
+			if mapData[int(p.view.position.X)][int(p.view.position.Y+p.view.direction.Y*s)] == 0 {
+				p.view.position.Y += p.view.direction.Y * s
+			}
+		}
+	}
+}
 
-    }
+const forward_acceleration = 0.003
+const backward_acceleration = -0.003
+const max_velocity = 0.1
+const min_velocity = -0.1
 
+func (p *Player) accelerateForward() {
+	p.view.velocity += forward_acceleration
+
+	if p.view.velocity > max_velocity {
+		p.view.velocity = max_velocity
+	}
+
+	p.moveForward(float64(p.view.velocity))
+}
+
+func (p *Player) accelerateBackward() {
+	p.view.velocity -= backward_acceleration
+
+	if p.view.velocity < min_velocity {
+		p.view.velocity = min_velocity
+	}
+
+	p.moveBackwards(float64(p.view.velocity))
+}
+
+func (p *Player) accelerateLeft() {
+	p.view.horizontalVelocity += forward_acceleration
+
+	if p.view.horizontalVelocity > max_velocity {
+		p.view.horizontalVelocity = max_velocity
+	}
+
+	p.moveLeft(float64(p.view.horizontalVelocity))
+}
+
+func (p *Player) accelerateRight() {
+	p.view.horizontalVelocity += forward_acceleration
+
+	if p.view.horizontalVelocity > max_velocity {
+		p.view.horizontalVelocity = max_velocity
+	}
+
+	p.moveRight(float64(p.view.horizontalVelocity))
 }
 
 func (p *Player) moveLeft(s float64) {
-    mapData := p.game.mapData
-    _, ok := interface{}(mapData).([][]int)
+	mapData := p.game.mapData
+	_, ok := interface{}(mapData).([][]int)
 
-    if ok {
-        if mapData[int(p.view.position.X-p.view.plane.X*s)][int(p.view.position.Y)] == 0 {
-            p.view.position.X -= p.view.plane.X * s
-        }
+	if ok {
+		if mapData[int(p.view.position.X-p.view.plane.X*s)][int(p.view.position.Y)] == 0 {
+			p.view.position.X -= p.view.plane.X * s
+		}
 
-        if mapData[int(p.view.position.X)][int(p.view.position.Y-p.view.plane.Y*s)] == 0 {
-            p.view.position.Y -= p.view.plane.Y * s
-        }
-    }
+		if mapData[int(p.view.position.X)][int(p.view.position.Y-p.view.plane.Y*s)] == 0 {
+			p.view.position.Y -= p.view.plane.Y * s
+		}
+	}
 }
 
 func (p *Player) moveBackwards(s float64) {
-    mapData := p.game.mapData
-    _, ok := interface{}(mapData).([][]int)
+	mapData := p.game.mapData
+	_, ok := interface{}(mapData).([][]int)
 
-    if ok {
-        if mapData[int(p.view.position.X-p.view.direction.X*s)][int(p.view.position.Y)] == 0 {
-            p.view.position.X -= p.view.direction.X * s
-        }
+	if ok {
+		// Check if new position is within map bounds
+		newX := int(p.view.position.X - p.view.direction.X*s)
+		if newX >= 0 && newX < len(mapData) {
+			// Check if new position is empty space
+			if mapData[newX][int(p.view.position.Y)] == 0 {
+				p.view.position.X -= p.view.direction.X * s
+			}
+		}
 
-        if mapData[int(p.view.position.X)][int(p.view.position.Y-p.view.direction.Y*s)] == 0 {
-            p.view.position.Y -= p.view.direction.Y * s
-        }
-    }
+		// Check if new position is within map bounds
+		newY := int(p.view.position.Y - p.view.direction.Y*s)
+		if newY >= 0 && newY < len(mapData[0]) {
+			// Check if new position is empty space
+			if mapData[int(p.view.position.X)][newY] == 0 {
+				p.view.position.Y -= p.view.direction.Y * s
+			}
+		}
+	}
 }
 
 func (p *Player) moveRight(s float64) {
-    mapData := p.game.mapData
-    _, ok := interface{}(mapData).([][]int)
+	mapData := p.game.mapData
+	_, ok := interface{}(mapData).([][]int)
 
-    if ok {
-        if mapData[int(p.view.position.X+p.view.plane.X*s)][int(p.view.position.Y)] == 0 {
-            p.view.position.X += p.view.plane.X * s
-        }
+	if ok {
+		if mapData[int(p.view.position.X+p.view.plane.X*s)][int(p.view.position.Y)] == 0 {
+			p.view.position.X += p.view.plane.X * s
+		}
 
-        if mapData[int(p.view.position.X)][int(p.view.position.Y+p.view.plane.Y*s)] == 0 {
-            p.view.position.Y += p.view.plane.Y * s
-        }
-    }
+		if mapData[int(p.view.position.X)][int(p.view.position.Y+p.view.plane.Y*s)] == 0 {
+			p.view.position.Y += p.view.plane.Y * s
+		}
+	}
 }
 
 func (p *Player) turnRight(s float64) {
-    oldDirX := p.view.direction.X
+	oldDirX := p.view.direction.X
 
-    p.view.direction.X = p.view.direction.X*math.Cos(-s) - p.view.direction.Y*math.Sin(-s)
-    p.view.direction.Y = oldDirX*math.Sin(-s) + p.view.direction.Y*math.Cos(-s)
+	p.view.direction.X = p.view.direction.X*math.Cos(-s) - p.view.direction.Y*math.Sin(-s)
+	p.view.direction.Y = oldDirX*math.Sin(-s) + p.view.direction.Y*math.Cos(-s)
 
-    oldPlaneX := p.view.plane.X
+	oldPlaneX := p.view.plane.X
 
-    p.view.plane.X = p.view.plane.X*math.Cos(-s) - p.view.plane.Y*math.Sin(-s)
-    p.view.plane.Y = oldPlaneX*math.Sin(-s) + p.view.plane.Y*math.Cos(-s)
+	p.view.plane.X = p.view.plane.X*math.Cos(-s) - p.view.plane.Y*math.Sin(-s)
+	p.view.plane.Y = oldPlaneX*math.Sin(-s) + p.view.plane.Y*math.Cos(-s)
 }
 
 func (p *Player) turnLeft(s float64) {
-    oldDirX := p.view.direction.X
+	oldDirX := p.view.direction.X
 
-    p.view.direction.X = p.view.direction.X*math.Cos(s) - p.view.direction.Y*math.Sin(s)
-    p.view.direction.Y = oldDirX*math.Sin(s) + p.view.direction.Y*math.Cos(s)
+	p.view.direction.X = p.view.direction.X*math.Cos(s) - p.view.direction.Y*math.Sin(s)
+	p.view.direction.Y = oldDirX*math.Sin(s) + p.view.direction.Y*math.Cos(s)
 
-    oldPlaneX := p.view.plane.X
+	oldPlaneX := p.view.plane.X
 
-    p.view.plane.X = p.view.plane.X*math.Cos(s) - p.view.plane.Y*math.Sin(s)
-    p.view.plane.Y = oldPlaneX*math.Sin(s) + p.view.plane.Y*math.Cos(s)
+	p.view.plane.X = p.view.plane.X*math.Cos(s) - p.view.plane.Y*math.Sin(s)
+	p.view.plane.Y = oldPlaneX*math.Sin(s) + p.view.plane.Y*math.Cos(s)
 }
 
 func (p *Player) getReward() float64 {
-    // Calculate euclidian distance between player 1 and player 2
-    p1Position := p.view.position
-    p2Position := p.game.player2Controller.player.view.position
+	// Calculate euclidian distance between player 1 and player 2
+	p1Position := p.view.position
+	p2Position := p.game.player2Controller.player.view.position
 
-    eucDistance := math.Sqrt(math.Pow(p1Position.X-p2Position.X, 2) + math.Pow(p1Position.Y-p2Position.Y, 2))
+	eucDistance := math.Sqrt(math.Pow(p1Position.X-p2Position.X, 2) + math.Pow(p1Position.Y-p2Position.Y, 2))
 
-    // Calculate reward that is inversely proportional to the euclidian distance
-    reward := 1 / eucDistance
-    return reward
+	// Calculate reward that is inversely proportional to the euclidian distance
+	reward := 1 / eucDistance
+	return reward
+}
+
+func (p *Player) getIntensityValuesAroundPlayer(m *Map, enemyPos, playerPos []int, soundData [][]float64) [][]float64 {
+	intensity := getIntensityValues(m, enemyPos, playerPos, soundData)
+
+	// Filter intensity values to only include values in a 1 grid cell radius around player's position
+	filteredIntensity := [][]float64{}
+	for i := playerPos[0] - 1; i <= playerPos[0]+1; i++ {
+		row := []float64{}
+		for j := playerPos[1] - 1; j <= playerPos[1]+1; j++ {
+			if i >= 0 && i < m.rows && j >= 0 && j < m.cols {
+				row = append(row, intensity[i][j])
+			}
+		}
+		filteredIntensity = append(filteredIntensity, row)
+	}
+
+	return filteredIntensity
+}
+
+func getIntensityValues(m *Map, enemyPos, playerPos []int, soundData [][]float64) [][]float64 {
+	intensity := [][]float64{}
+	for i := 0; i < m.rows; i++ {
+		intensity = append(intensity, make([]float64, m.cols))
+	}
+
+	// Calculate intensity of sound at each grid cell based on distance from source and obstacles
+	for i := 0; i < m.rows; i++ {
+		for j := 0; j < m.cols; j++ {
+			intensity[i][j] = calcIntensity(i, j, playerPos[0], playerPos[1], m)
+		}
+	}
+
+	return intensity
+}
+
+func calcIntensity(x, y, sourceX, sourceY int, m *Map) float64 {
+	// Calculate intensity of sound at (x, y) based on distance from source and obstacles
+	// For example, using the inverse square law:
+	distance := math.Sqrt(math.Pow(float64(x-sourceX), 2) + math.Pow(float64(y-sourceY), 2))
+	intensity := 1.0 / math.Pow(distance, 2)
+
+	// Adjust intensity based on obstacles in the way
+	for i := x - 1; i <= x+1; i++ {
+		for j := y - 1; j <= y+1; j++ {
+			if i >= 0 && i < m.rows && j >= 0 && j < m.cols && m.mapData[i][j] == 2 {
+				intensity *= 0.5
+			}
+		}
+	}
+
+	return intensity
 }
--- game/render.go
+++ game/render.go
@@ -1,193 +1,200 @@
 package game
 
+import "C"
 import (
-	"github.com/faiface/pixel"
-	"image"
-	"math"
+    "github.com/faiface/pixel"
+    "image"
+    "math"
 )
 
 const texSize = 64
 
 type RenderView struct {
-	parent         *Player
-	renderListener *RenderListener
+    parent         *Player
+    renderListener *RenderListener
 
-	renderWidth  int
-	renderHeight int
+    renderWidth  int
+    renderHeight int
 
-	direction pixel.Vec
-	position  pixel.Vec
-	plane     pixel.Vec
+    direction pixel.Vec
+    position  pixel.Vec
+    plane     pixel.Vec
 
-	distanceToWall float64 // Calculated after a render cycle
+    velocity           float32
+    horizontalVelocity float32
+
+    distanceToWall float64 // Calculated after a render cycle
 }
 
 type RenderListener struct {
-	renderBuffer *image.RGBA
+    renderBuffer *image.RGBA
 }
 
 func (c *RenderView) render() *image.RGBA {
-	m := image.NewRGBA(image.Rect(0, 0, c.renderWidth, c.renderHeight))
+    m := image.NewRGBA(image.Rect(0, 0, c.renderWidth, c.renderHeight))
 
-	for x := 0; x < c.renderWidth; x++ {
-		var (
-			step         image.Point
-			sideDist     pixel.Vec
-			perpWallDist float64
-			hit, side    bool
+    for x := 0; x < c.renderWidth; x++ {
+        var (
+            step         image.Point
+            sideDist     pixel.Vec
+            perpWallDist float64
+            hit, side    bool
 
-			worldX, worldY = int(c.position.X), int(c.position.Y)
+            worldX, worldY = int(c.position.X), int(c.position.Y)
 
-			cameraX = 2*float64(x)/float64(c.renderWidth) - 1
+            cameraX = 2*float64(x)/float64(c.renderWidth) - 1
 
-			rayDir = pixel.V(
-				c.direction.X+c.plane.X*cameraX,
-				c.direction.Y+c.plane.Y*cameraX,
-			)
+            rayDir = pixel.V(
+                c.direction.X+c.plane.X*cameraX,
+                c.direction.Y+c.plane.Y*cameraX,
+            )
 
-			deltaDist = pixel.V(
-				math.Sqrt(1.0+(rayDir.Y*rayDir.Y)/(rayDir.X*rayDir.X)),
-				math.Sqrt(1.0+(rayDir.X*rayDir.X)/(rayDir.Y*rayDir.Y)),
-			)
-		)
+            deltaDist = pixel.V(
+                math.Sqrt(1.0+(rayDir.Y*rayDir.Y)/(rayDir.X*rayDir.X)),
+                math.Sqrt(1.0+(rayDir.X*rayDir.X)/(rayDir.Y*rayDir.Y)),
+            )
+        )
 
-		if rayDir.X < 0 {
-			step.X = -1
-			sideDist.X = (c.position.X - float64(int(c.position.X))) * deltaDist.X
-		} else {
-			step.X = 1
-			sideDist.X = (float64(int(c.position.X)) + 1.0 - c.position.X) * deltaDist.X
-		}
+        if rayDir.X < 0 {
+            step.X = -1
+            sideDist.X = (c.position.X - float64(int(c.position.X))) * deltaDist.X
+        } else {
+            step.X = 1
+            sideDist.X = (float64(int(c.position.X)) + 1.0 - c.position.X) * deltaDist.X
+        }
 
-		if rayDir.Y < 0 {
-			step.Y = -1
-			sideDist.Y = (c.position.Y - float64(int(c.position.Y))) * deltaDist.Y
-		} else {
-			step.Y = 1
-			sideDist.Y = (float64(int(c.position.Y)) + 1.0 - c.position.Y) * deltaDist.Y
-		}
+        if rayDir.Y < 0 {
+            step.Y = -1
+            sideDist.Y = (c.position.Y - float64(int(c.position.Y))) * deltaDist.Y
+        } else {
+            step.Y = 1
+            sideDist.Y = (float64(int(c.position.Y)) + 1.0 - c.position.Y) * deltaDist.Y
+        }
 
-		for !hit {
-			if sideDist.X < sideDist.Y {
-				sideDist.X += deltaDist.X
-				worldX += step.X
-				side = false
-			} else {
-				sideDist.Y += deltaDist.Y
-				worldY += step.Y
-				side = true
-			}
+        for !hit {
+            if sideDist.X < sideDist.Y {
+                sideDist.X += deltaDist.X
+                worldX += step.X
+                side = false
+            } else {
+                sideDist.Y += deltaDist.Y
+                worldY += step.Y
+                side = true
+            }
 
-			if c.parent.game.mapData[worldX][worldY] > 0 {
-				hit = true
-			}
-			if c.parent.game.mapData[worldX][worldY] == 3 {
-				hit = false
-			}
-		}
+            if c.parent.game.mapData[worldX][worldY] > 0 {
+                hit = true
+            }
+            if c.parent.game.mapData[worldX][worldY] == 3 {
+                hit = false
+            }
+        }
 
-		var wallX float64
+        var wallX float64
 
-		if side {
-			perpWallDist = (float64(worldY) - c.position.Y + (1-float64(step.Y))/2) / rayDir.Y
-			wallX = c.position.X + perpWallDist*rayDir.X
-		} else {
-			perpWallDist = (float64(worldX) - c.position.X + (1-float64(step.X))/2) / rayDir.X
-			wallX = c.position.Y + perpWallDist*rayDir.Y
-		}
+        if side {
+            perpWallDist = (float64(worldY) - c.position.Y + (1-float64(step.Y))/2) / rayDir.Y
+            wallX = c.position.X + perpWallDist*rayDir.X
+        } else {
+            perpWallDist = (float64(worldX) - c.position.X + (1-float64(step.X))/2) / rayDir.X
+            wallX = c.position.Y + perpWallDist*rayDir.Y
+        }
 
-		if x == c.renderWidth/2 {
-			c.distanceToWall = perpWallDist
-		}
+        if x == c.renderWidth/2 {
+            c.distanceToWall = perpWallDist
+        }
 
-		wallX -= math.Floor(wallX)
+        wallX -= math.Floor(wallX)
 
-		texX := int(wallX * float64(texSize))
+        texX := int(wallX * float64(texSize))
 
-		lineHeight := int(float64(c.renderHeight) / perpWallDist)
+        lineHeight := int(float64(c.renderHeight) / perpWallDist)
 
-		if lineHeight < 1 {
-			lineHeight = 1
-		}
+        if lineHeight < 1 {
+            lineHeight = 1
+        }
 
-		drawStart := -lineHeight/2 + c.renderHeight/2
-		if drawStart < 0 {
-			drawStart = 0
-		}
+        drawStart := -lineHeight/2 + c.renderHeight/2
+        if drawStart < 0 {
+            drawStart = 0
+        }
 
-		drawEnd := lineHeight/2 + c.renderHeight/2
-		if drawEnd >= c.renderHeight {
-			drawEnd = c.renderHeight - 1
-		}
+        drawEnd := lineHeight/2 + c.renderHeight/2
+        if drawEnd >= c.renderHeight {
+            drawEnd = c.renderHeight - 1
+        }
 
-		if !side && rayDir.X > 0 {
-			texX = texSize - texX - 1
-		}
+        if !side && rayDir.X > 0 {
+            texX = texSize - texX - 1
+        }
 
-		if side && rayDir.Y < 0 {
-			texX = texSize - texX - 1
-		}
+        if side && rayDir.Y < 0 {
+            texX = texSize - texX - 1
+        }
 
-		texNum := c.parent.game.getTexNum(worldX, worldY)
+        texNum := c.parent.game.getTexNum(worldX, worldY)
 
-		for y := drawStart; y < drawEnd+1; y++ {
-			d := y*256 - c.renderHeight*128 + lineHeight*128
-			texY := ((d * texSize) / lineHeight) / 256
+        for y := drawStart; y < drawEnd+1; y++ {
+            d := y*256 - c.renderHeight*128 + lineHeight*128
+            texY := ((d * texSize) / lineHeight) / 256
 
-			c := c.parent.game.textureMap.RGBAAt(
-				texX+texSize*(texNum),
-				texY%texSize,
-			)
+            c := c.parent.game.textureMap.RGBAAt(
+                texX+texSize*(texNum),
+                texY%texSize,
+            )
 
-			if side {
-				c.R = c.R / 2
-				c.G = c.G / 2
-				c.B = c.B / 2
-			}
+            if side {
+                c.R = c.R / 2
+                c.G = c.G / 2
+                c.B = c.B / 2
+            }
 
-			m.Set(x, y, c)
-		}
+            m.Set(x, y, c)
+        }
 
-		var floorWall pixel.Vec
+        var floorWall pixel.Vec
 
-		if !side && rayDir.X > 0 {
-			floorWall.X = float64(worldX)
-			floorWall.Y = float64(worldY) + wallX
-		} else if !side && rayDir.X < 0 {
-			floorWall.X = float64(worldX) + 1.0
-			floorWall.Y = float64(worldY) + wallX
-		} else if side && rayDir.Y > 0 {
-			floorWall.X = float64(worldX) + wallX
-			floorWall.Y = float64(worldY)
-		} else {
-			floorWall.X = float64(worldX) + wallX
-			floorWall.Y = float64(worldY) + 1.0
-		}
+        if !side && rayDir.X > 0 {
+            floorWall.X = float64(worldX)
+            floorWall.Y = float64(worldY) + wallX
+        } else if !side && rayDir.X < 0 {
+            floorWall.X = float64(worldX) + 1.0
+            floorWall.Y = float64(worldY) + wallX
+        } else if side && rayDir.Y > 0 {
+            floorWall.X = float64(worldX) + wallX
+            floorWall.Y = float64(worldY)
+        } else {
+            floorWall.X = float64(worldX) + wallX
+            floorWall.Y = float64(worldY) + 1.0
+        }
 
-		distWall, distPlayer := perpWallDist, 0.0
+        distWall, distPlayer := perpWallDist, 0.0
 
-		for y := drawEnd + 1; y < c.renderHeight; y++ {
-			currentDist := float64(c.renderHeight) / (2.0*float64(y) - float64(c.renderHeight))
+        for y := drawEnd + 1; y < c.renderHeight; y++ {
+            currentDist := float64(c.renderHeight) / (2.0*float64(y) - float64(c.renderHeight))
 
-			weight := (currentDist - distPlayer) / (distWall - distPlayer)
+            weight := (currentDist - distPlayer) / (distWall - distPlayer)
 
-			currentFloor := pixel.V(
-				weight*floorWall.X+(1.0-weight)*c.position.X,
-				weight*floorWall.Y+(1.0-weight)*c.position.Y,
-			)
+            currentFloor := pixel.V(
+                weight*floorWall.X+(1.0-weight)*c.position.X,
+                weight*floorWall.Y+(1.0-weight)*c.position.Y,
+            )
 
-			fx := int(currentFloor.X*float64(texSize)) % texSize
-			fy := int(currentFloor.Y*float64(texSize)) % texSize
+            fx := int(currentFloor.X*float64(texSize)) % texSize
+            fy := int(currentFloor.Y*float64(texSize)) % texSize
 
-			m.Set(x, y, c.parent.game.textureMap.At(fx, fy))
+            m.Set(x, y, c.parent.game.textureMap.At(fx, fy))
 
-			m.Set(x, c.renderHeight-y-1, c.parent.game.textureMap.At(fx+(4*texSize), fy))
-			m.Set(x, c.renderHeight-y, c.parent.game.textureMap.At(fx+(4*texSize), fy))
-		}
-	}
+            m.Set(x, c.renderHeight-y-1, c.parent.game.textureMap.At(fx+(4*texSize), fy))
+            m.Set(x, c.renderHeight-y, c.parent.game.textureMap.At(fx+(4*texSize), fy))
+        }
+    }
 
-	if c.renderListener != nil {
-		c.renderListener.renderBuffer = m
-	}
-	return m
+    // Add code here to render the enemy position as a floating sphere
+    // based on the enemy's position and the player's position
+
+    if c.renderListener != nil {
+        c.renderListener.renderBuffer = m
+    }
+    return m
 }
